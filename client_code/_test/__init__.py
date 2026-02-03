24709d68a8e4119008333cd065b2aed5560a2fe0
sassycomapp@gmail.com <sassycomapp@gmail.com>
2026-02-03 01:58:40 +0000

    Created _test

---
+++ client_code/_test/__init__.py
@@ -0,0 +1,20 @@
+from ._anvil_designer import _testTemplate
+from anvil import *
+import anvil.server
+import m3.components as m3
+from routing import router
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import stripe.checkout
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class _test(_testTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
